c3e3b98fe3621c7549da25fd1337ab74cd926394
nflsilva <nelsonflsilva@gmail.com>
2021-01-03 00:55:39 +0000

    Fixed aspect ratio. Added multiple lights.

--- main.py
+++ main.py
@@ -42,11 +42,13 @@ def main():
     camera = Camera(x=0, y=0, z=-1.5)
     screen = Screen(bottom=-1 / aspect_ratio, top=1 / aspect_ratio, left=1, right=-1, width=width, height=height)
 
-    light = Light(x=-4, y=-4, z=-2, material=Material(ambient=Color(1, 1, 1), diffuse=Color(1, 1, 1), specular=Color(1, 1, 1)))
+    scene_lights = [
+        Light(x=4, y=-2, z=-2, material=Material(ambient=Color(.1, .1, .1), diffuse=Color(.7, .7, .7), specular=Color(1.0, 1.0, 1.0)))
+    ]
 
     scene_objects = [
-        Sphere(origin=np.array([0, 0, 5]), radius=1.0,
-               material=Material(ambient=Color(0.1, 0, 0), diffuse=Color(0.7, 0, 0), specular=Color(1, 1, 1), shininess=100))
+        Sphere(origin=np.array([0, 0, 5]), radius=1.0, material=Material(ambient=Color(.1, .0, .0), diffuse=Color(.7, .0, .0), specular=Color(1, 1, 1), shininess=100)),
+        Sphere(origin=np.array([1.0, 0, 3.5]), radius=0.15, material=Material(ambient=Color(.0, .1, .0), diffuse=Color(.0, .7, .0), specular=Color(1, 1, 1), shininess=25))
     ]
 
 
@@ -70,29 +72,29 @@ def main():
             intersection_normal = nearest_obj.get_intersection_normal(ray, intersection_point)
             shifted_nearest_obj_intersection_point = intersection_point + 1e-10 * intersection_normal
 
-            light_ray = Ray(origin=shifted_nearest_obj_intersection_point, destination=light.position)
-            light_ray_intersection_distance, _ = find_nearest_object_to_ray(scene_objects, light_ray)
-            intersection_to_light_distance = np.linalg.norm(light.position - intersection_point)
-            is_shadowed = light_ray_intersection_distance < intersection_to_light_distance
-
-            if is_shadowed:
-                continue
-
             # RGB
             illumination = np.zeros((3))
+            for light in scene_lights:
 
+                light_ray = Ray(origin=shifted_nearest_obj_intersection_point, destination=light.position)
+                light_ray_intersection_distance, _ = find_nearest_object_to_ray(scene_objects, light_ray)
+                intersection_to_light_distance = np.linalg.norm(light.position - intersection_point)
+                is_shadowed = light_ray_intersection_distance < intersection_to_light_distance
 
-            # ambient
-            illumination += nearest_obj.material.ambient * light.material.ambient
+                if is_shadowed:
+                    continue
 
-            # diffuse
-            illumination += nearest_obj.material.diffuse * light.material.diffuse * np.dot(light_ray.direction, intersection_normal)
+                # ambient
+                illumination += nearest_obj.material.ambient * light.material.ambient
 
-            # specular
-            intersection_to_camera = normalize(camera.position - intersection_point)
-            H = normalize(light_ray.direction + intersection_to_camera)
-            illumination += nearest_obj.material.specular * light.material.specular * np.dot(intersection_normal, H) \
-                            ** (nearest_obj.material.shininess / 4)
+                # diffuse
+                illumination += nearest_obj.material.diffuse * light.material.diffuse * np.dot(light_ray.direction, intersection_normal)
+
+                # specular
+                intersection_to_camera = normalize(camera.position - intersection_point)
+                H = normalize(light_ray.direction + intersection_to_camera)
+                illumination += nearest_obj.material.specular * light.material.specular * np.dot(intersection_normal, H) \
+                                ** (nearest_obj.material.shininess / 4)
 
             illumination = np.clip(illumination, 0, 1)
 
--- scomponent/Screen.py
+++ scomponent/Screen.py
@@ -18,6 +18,9 @@ class Screen:
     def set_pixel_color(self, x, y, color):
         self.image[y, x] = (color.r, color.g, color.b)
 
+    def apply_antialiasing(self):
+        
+
 
     def write_to_file(self):
         plt.imsave("output.png", self.image)
